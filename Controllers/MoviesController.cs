fb683849278e72bad5dfbac9939248fb7fb5173a
Tikane <tikane.kgoatla1@gmail.com>
2021-09-28 20:59:16 +0200

    Add project files.

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using JoeMovies.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JoeMovies.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Ben 10"
+            };
+
+            return View(movie);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JoeMovies.MvcApplication" Language="C#" %>
