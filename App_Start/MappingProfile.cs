d71e9342d422b1bca1f04924076f6935b91e91f0
Tikane <tikane.kgoatla1@gmail.com>
2021-10-01 20:26:45 +0200

    Section 6: Movie API

--- App_Start/MappingProfile.cs
+++ App_Start/MappingProfile.cs
@@ -12,8 +12,13 @@ namespace JoeMovies.App_Start
     {
         public MappingProfile()
         {
+            //---- Customer
             Mapper.CreateMap<Customer,CustomerDto>();
             Mapper.CreateMap<CustomerDto, Customer>();
+
+            //---- Movie
+            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<MovieDto, Movie>();
         }
     }
 }
